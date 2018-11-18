c42c88c959da7cd84571cd45545f3715953eaf4c
LAPTOP-9DQS12E1\Henry Ding <henry1998628@gmail.com>
2018-11-17 17:58:04 -0800

    Restore things from the previous repo

---
+++ Captain Bee/Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+
+    private Slider slider;
+
+    private GameObject player;
+
+
+    public float time = 10f;
+
+    [SerializeField]
+    private float decay = 0.3f;
+
+    [SerializeField]
+    private string levelName = "Sidescrolling";
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
+        slider.minValue = 0f;
+        slider.maxValue = time;
+        slider.value = slider.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= decay * Time.deltaTime;
+            slider.value = time;
+        }
+        else
+        {
+            Destroy(player);
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
